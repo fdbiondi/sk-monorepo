7e310c788e2bf3e3681153c30ca1d25c169f8738
fbiondi <fdbion@gmail.com>
2024-03-25 14:29:10 -0300

    remove functions from seeds script

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -209,21 +209,15 @@ INSERT INTO "storage"."objects" ("id", "bucket_id", "name", "owner", "created_at
 -- Name: refresh_tokens_id_seq; Type: SEQUENCE SET; Schema: auth; Owner: supabase_auth_admin
 --
 
-SELECT pg_catalog.setval('"auth"."refresh_tokens_id_seq"', 1, true);
-
 
 --
 -- Name: key_key_id_seq; Type: SEQUENCE SET; Schema: pgsodium; Owner: supabase_admin
 --
 
-SELECT pg_catalog.setval('"pgsodium"."key_key_id_seq"', 1, false);
-
 
 --
 -- Name: hooks_id_seq; Type: SEQUENCE SET; Schema: supabase_functions; Owner: supabase_functions_admin
 --
-
-SELECT pg_catalog.setval('"supabase_functions"."hooks_id_seq"', 1, false);
 
 
 --
