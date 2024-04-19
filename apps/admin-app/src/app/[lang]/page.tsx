fc040891b001eae6621ef0d8e08340c8453e92d2
fbiondi <fdbion@gmail.com>
2024-04-19 11:48:17 -0300

    setup drizzle orm package

--- apps/admin-app/src/app/[lang]/page.tsx
+++ apps/admin-app/src/app/[lang]/page.tsx
@@ -1,6 +1,11 @@
 import { redirect } from 'next/navigation';
 
+// export default async function Page() {
 export default function Page() {
+  // const allProducts = await db.select().from(products);
+
+  // console.log({ allProducts });
+
   redirect('/dashboard');
 
   return <p>Landing</p>;
---
+++ packages/orm/README.md
@@ -0,0 +1,3 @@
+# orm
+
+This library was generated with [Nx](https://nx.dev).
