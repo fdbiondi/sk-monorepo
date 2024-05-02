9d59b9ce51bd4fccb3a2cbdf05b087d44ce9dfe3
fbiondi <fdbion@gmail.com>
2024-05-02 17:43:17 -0300

    containers tables

---
+++ apps/supabase/migrations/20240502203825_containers_table.sql
@@ -0,0 +1,64 @@
+create type "public"."container_type" as enum ('pre_recorded', 'bayg');
+
+create table "public"."containers" (
+    "id" uuid not null default gen_random_uuid(),
+    "product_id" uuid not null,
+    "type" container_type not null,
+    "created_at" timestamp with time zone not null default now(),
+    "updated_at" timestamp with time zone default now()
+);
+
+
+alter table "public"."containers" enable row level security;
+
+CREATE UNIQUE INDEX containers_pkey ON public.containers USING btree (id);
+
+alter table "public"."containers" add constraint "containers_pkey" PRIMARY KEY using index "containers_pkey";
+
+alter table "public"."containers" add constraint "containers_product_id_fkey" FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE not valid;
+
+alter table "public"."containers" validate constraint "containers_product_id_fkey";
+
+grant delete on table "public"."containers" to "anon";
+
+grant insert on table "public"."containers" to "anon";
+
+grant references on table "public"."containers" to "anon";
+
+grant select on table "public"."containers" to "anon";
+
+grant trigger on table "public"."containers" to "anon";
+
+grant truncate on table "public"."containers" to "anon";
+
+grant update on table "public"."containers" to "anon";
+
+grant delete on table "public"."containers" to "authenticated";
+
+grant insert on table "public"."containers" to "authenticated";
+
+grant references on table "public"."containers" to "authenticated";
+
+grant select on table "public"."containers" to "authenticated";
+
+grant trigger on table "public"."containers" to "authenticated";
+
+grant truncate on table "public"."containers" to "authenticated";
+
+grant update on table "public"."containers" to "authenticated";
+
+grant delete on table "public"."containers" to "service_role";
+
+grant insert on table "public"."containers" to "service_role";
+
+grant references on table "public"."containers" to "service_role";
+
+grant select on table "public"."containers" to "service_role";
+
+grant trigger on table "public"."containers" to "service_role";
+
+grant truncate on table "public"."containers" to "service_role";
+
+grant update on table "public"."containers" to "service_role";
+
+
---
+++ apps/supabase/migrations/20240502204121_containers_lessons_table.sql
@@ -0,0 +1,67 @@
+create table "public"."containers_lessons" (
+    "id" uuid not null default gen_random_uuid(),
+    "container_id" uuid not null,
+    "lesson_id" uuid not null,
+    "order" bigint not null,
+    "created_at" timestamp with time zone not null default now(),
+    "updated_at" timestamp with time zone default now()
+);
+
+
+alter table "public"."containers_lessons" enable row level security;
+
+CREATE UNIQUE INDEX containers_lessons_pkey ON public.containers_lessons USING btree (id);
+
+alter table "public"."containers_lessons" add constraint "containers_lessons_pkey" PRIMARY KEY using index "containers_lessons_pkey";
+
+alter table "public"."containers_lessons" add constraint "containers_lessons_container_id_fkey" FOREIGN KEY (container_id) REFERENCES containers(id) ON DELETE CASCADE not valid;
+
+alter table "public"."containers_lessons" validate constraint "containers_lessons_container_id_fkey";
+
+alter table "public"."containers_lessons" add constraint "containers_lessons_lesson_id_fkey" FOREIGN KEY (lesson_id) REFERENCES lessons(id) ON DELETE CASCADE not valid;
+
+alter table "public"."containers_lessons" validate constraint "containers_lessons_lesson_id_fkey";
+
+grant delete on table "public"."containers_lessons" to "anon";
+
+grant insert on table "public"."containers_lessons" to "anon";
+
+grant references on table "public"."containers_lessons" to "anon";
+
+grant select on table "public"."containers_lessons" to "anon";
+
+grant trigger on table "public"."containers_lessons" to "anon";
+
+grant truncate on table "public"."containers_lessons" to "anon";
+
+grant update on table "public"."containers_lessons" to "anon";
+
+grant delete on table "public"."containers_lessons" to "authenticated";
+
+grant insert on table "public"."containers_lessons" to "authenticated";
+
+grant references on table "public"."containers_lessons" to "authenticated";
+
+grant select on table "public"."containers_lessons" to "authenticated";
+
+grant trigger on table "public"."containers_lessons" to "authenticated";
+
+grant truncate on table "public"."containers_lessons" to "authenticated";
+
+grant update on table "public"."containers_lessons" to "authenticated";
+
+grant delete on table "public"."containers_lessons" to "service_role";
+
+grant insert on table "public"."containers_lessons" to "service_role";
+
+grant references on table "public"."containers_lessons" to "service_role";
+
+grant select on table "public"."containers_lessons" to "service_role";
+
+grant trigger on table "public"."containers_lessons" to "service_role";
+
+grant truncate on table "public"."containers_lessons" to "service_role";
+
+grant update on table "public"."containers_lessons" to "service_role";
+
+
