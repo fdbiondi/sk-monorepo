67f530ba4b403f6b1eb4e35450a14656e35961e5
fbiondi <fdbion@gmail.com>
2024-05-04 10:43:35 -0300

    remove comments

--- apps/edge-api/src/resolvers/mocks/mockQuery.ts
+++ apps/edge-api/src/resolvers/mocks/mockQuery.ts
@@ -1,5 +1,4 @@
 import { extractFromResponse } from '../../helpers';
-// import { AppError } from '../../models/errors';
 import { Context } from '../../typings';
 
 export const mockQuery = async (
@@ -7,17 +6,7 @@ export const mockQuery = async (
   _args: unknown,
   context: Context
 ) => {
-  // if (context.mustRespondWithMock) {
-
   const { data } = await context.fetchMockApi<{ data: unknown }>();
 
-  return extractFromResponse(data, '');
-
-  // }
-  //
-  // if (context.request.user === undefined) {
-  //   throw new AppError(undefined, 'UNAUTHORIZED');
-  // }
-  //
-  // return [];
+  return extractFromResponse(data);
 };
